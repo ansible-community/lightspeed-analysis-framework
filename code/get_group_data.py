9d746264ce71fcfc213e2780656a3d0c7c7cf81e
priyamsahoo <priyamsahoo.26@gmail.com>
2023-10-01 00:14:59 +0530

    collect group data

---
+++ code/get_group_data.py
@@ -0,0 +1,27 @@
+def get_group_data(file, group):
+    """Extract data of a particular group from the filtered data file
+
+    Args:
+        file (string): file uri of the filtered data file
+        group (strip): group name
+
+    Returns:
+        string: file uri of the extracted group's data file
+    """
+
+
+    input_file_name = file
+    output_file_name = f"data/{group}_filtered.jsonl"
+
+    search_group_substring = group
+
+    with open(input_file_name, "r") as input_file, open(output_file_name, "w") as output_file:
+        for line in input_file:
+            if search_group_substring in line:
+                output_file.write(line)
+
+    # Close both files
+    input_file.close()
+    output_file.close()
+
+    return output_file_name
