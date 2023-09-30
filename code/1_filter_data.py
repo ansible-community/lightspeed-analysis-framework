edb07ca0c169b344e3168ee7bdcde8cda1163a2d
priyamsahoo <priyamsahoo.26@gmail.com>
2023-09-30 17:48:15 +0530

    filter raw file for necessary keys and event data

---
+++ code/1_filter_data.py
@@ -0,0 +1,58 @@
+import re
+import os
+
+
+def filter_data(file):
+    """Filter the necessary keys and event data from the raw data file
+
+    Args:
+        file (string): file uri of the raw data file
+
+    Returns:
+        string: file uri of the filtered data
+    """
+
+    current_directory = os.path.join(os.getcwd(), "code", "playground")
+    input_file_name = os.path.join(current_directory, "test_data.jsonl")
+    output_file_name = os.path.join(current_directory, "filtered_test_data.jsonl")
+
+    # input_file_name = file
+
+    # regex of unnecesasary keys
+    message_id_pattern = r'"messageId":"([a-f0-9\-]+)",'
+    project_id_pattern = r'"projectId":"([a-zA-Z0-9]+)",'
+    write_key_pattern = r',"writeKey":"([a-zA-Z0-9]+)"'
+    version_key_pattern = r'"version":([0-9]),'
+    image_tags_pattern = r'"imageTags":"(latest [\d].[\d].\d*)",'
+    hostname_pattern = r'"hostname":"([A-Za-z0-9\-]*)",'
+
+    # events to extract
+    search_substrings = ['"event":"inlineSuggestionFeedback"', '"event":"completion"', '"event":"ansibleContentFeedback"']
+
+
+    with open(input_file_name, "r") as input_file, open(output_file_name, "w") as output_file:
+        for line in input_file:
+            if any(substring in line for substring in search_substrings):
+
+                # remove unnecessary strings
+                line = line.replace('"anonymousId":null,"channel":"server","context":{"library":{"name":"analytics-python","version":"2.2.2"}},', '')
+                line = line.replace('"integrations":{},', '')
+                line = line.replace('"type":"track",', '')
+                line = line.replace('"modelName":"ansible-wisdom-v11",', '')
+                
+                # remove unnecessary keys
+                line = re.sub(message_id_pattern, "", line)
+                line = re.sub(project_id_pattern, "", line)
+                line = re.sub(version_key_pattern, "", line)
+                line = re.sub(write_key_pattern, "", line)
+                line = re.sub(image_tags_pattern, "", line)
+                line = re.sub(hostname_pattern, "", line)
+
+                # if any event substring is found, write the line to the output file
+                output_file.write(line)
+
+    # Close both files
+    input_file.close()
+    output_file.close()
+
+    return output_file_name
