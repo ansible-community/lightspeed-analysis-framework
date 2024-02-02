35db5a38e4f78648446e28ec1b86bc90a9285ae5
priyamsahoo <priyamsahoo.26@gmail.com>
2024-02-02 21:44:50 +0530

    update main file for data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,12 +1,23 @@
 import os
+import csv
 from analysis_code.combine_items import combine_items
 
 print("starting analysis...")
-user_data_folder = "data/user_files"
+user_data_folder = "test/user_files"
 
-for file in os.listdir(user_data_folder):
-    # print(file)
-    if(file.endswith(".jsonl")):
-        combine_items(user_data_folder, file)
-    else:
-        continue
+header = ["id", "suggestions_analyzed", "accepted_suggestion", "committed_suggestions", "fully_accepted", "major_edits", "minor_edits", "changed_key", "changed_value", "changed_module", "no_match", "deleted_after_accepting", "minor_edit_key_change", "minor_edit_value_change", "minor_edit_module_change"]
+
+with open('user_analysis.csv', 'w') as file:
+    writer = csv.DictWriter(file, fieldnames=header)
+    writer.writeheader()
+
+    for file in os.listdir(user_data_folder):
+        if(file.endswith(".jsonl")):
+            user_data = combine_items(user_data_folder, file)
+            user_data["id"] = file
+            
+            if(user_data["committed_suggestions"] != 0):
+                writer.writerow(user_data)
+
+        else:
+            continue
