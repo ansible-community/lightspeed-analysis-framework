529688ea35da6afaa633ed65647c1d46ea1433a9
priyamsahoo <priyamsahoo.26@gmail.com>
2024-02-03 10:13:43 +0530

    remove file name from id and put exact user id

--- data_analysis.py
+++ data_analysis.py
@@ -1,4 +1,5 @@
 import os
+import re
 import csv
 from analysis_code.combine_items import combine_items
 
@@ -13,6 +14,8 @@ header = ["id", "suggestions_analyzed", "accepted_suggestion",
         "no_match", "deleted_after_accepting", "minor_edit_key_change", 
         "minor_edit_value_change", "minor_edit_module_change"]
 
+user_id_pattern = r'sorted_(.*?)_filtered_file\.jsonl'
+
 with open(output_csv_file, 'w') as file:
     writer = csv.DictWriter(file, fieldnames=header)
     writer.writeheader()
@@ -20,7 +23,9 @@ with open(output_csv_file, 'w') as file:
     for file in os.listdir(user_data_folder):
         if(file.endswith(".jsonl")):
             user_data = combine_items(user_data_folder, file)
-            user_data["id"] = file
+            match = re.search(user_id_pattern, file)
+            if match:
+                user_data["id"] = match.group(1)
             
             if(user_data["committed_suggestions"] != 0):
                 writer.writerow(user_data)
